0e8aab5c37f8def3cf0b00970e041e88746b1f97
MarcinSzramka <szramka-marcin@wp.pl>
2020-10-26 13:53:54 +0100

    first commit

---
+++ VirtualSynth/MainInterface.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace VirtualSynth
+{
+    partial class MainInterface
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.osc1 = new VirtualSynth.OSC();
+            this.osc2 = new VirtualSynth.OSC();
+            this.osc3 = new VirtualSynth.OSC();
+            this.SuspendLayout();
+            // 
+            // osc1
+            // 
+            this.osc1.Location = new System.Drawing.Point(12, 12);
+            this.osc1.Name = "osc1";
+            this.osc1.Size = new System.Drawing.Size(412, 101);
+            this.osc1.TabIndex = 0;
+            this.osc1.TabStop = false;
+            this.osc1.Text = "osc1";
+            // 
+            // osc2
+            // 
+            this.osc2.Location = new System.Drawing.Point(12, 119);
+            this.osc2.Name = "osc2";
+            this.osc2.Size = new System.Drawing.Size(412, 127);
+            this.osc2.TabIndex = 1;
+            this.osc2.TabStop = false;
+            this.osc2.Text = "osc2";
+            // 
+            // osc3
+            // 
+            this.osc3.Location = new System.Drawing.Point(12, 252);
+            this.osc3.Name = "osc3";
+            this.osc3.Size = new System.Drawing.Size(412, 100);
+            this.osc3.TabIndex = 2;
+            this.osc3.TabStop = false;
+            this.osc3.Text = "osc3";
+            // 
+            // MainInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.osc3);
+            this.Controls.Add(this.osc2);
+            this.Controls.Add(this.osc1);
+            this.KeyPreview = true;
+            this.Name = "MainInterface";
+            this.Text = "Virtual Synthesizer";
+            this.Load += new System.EventHandler(this.MainInterface_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainInterface_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OSC osc1;
+        private OSC osc2;
+        private OSC osc3;
+    }
+}
+
---
+++ VirtualSynth/MainInterface.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+
+
+namespace VirtualSynth
+{
+    public partial class MainInterface : Form
+    {
+        public const int  sampleRate = 41000;
+        public const short bitsPerSample = 16;
+
+        public MainInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void MainInterface_KeyDown(object sender, KeyEventArgs e)
+        {
+            Random rnd = new Random();
+            short[] wave = new short[sampleRate];
+            byte[] binaryWave = new byte[sampleRate * sizeof(short) ];
+            float freq;
+        //Keys
+            switch(e.KeyCode)
+            {
+                case Keys.Z:
+                    freq = 65.4f;
+                    break;
+                case Keys.X:
+                    freq = 138.59f;
+                    break;
+                case Keys.C:
+                    freq = 261.62f;
+                    break;
+                case Keys.V:
+                    freq = 523.25f;
+                    break;
+                case Keys.B:
+                    freq = 1046.5f;
+                    break;
+                case Keys.N:
+                    freq = 2093f;
+                    break;
+                case Keys.M:
+                    freq = 4186.01f;
+                    break;
+                default:
+                    return;
+            }
+
+         // WaveForms
+            foreach (OSC oscillator in this.Controls.OfType<OSC>())
+            {
+                short tempSample;
+                int samplesPerWaveLength = (int)(sampleRate / freq);
+                short ampStep = (short)((short.MaxValue * 2) / samplesPerWaveLength);
+                switch (oscillator.WaveForm)
+                {
+                    case WaveForm.Sine:
+                      for (int i = 0; i < sampleRate; i++)
+                      {
+                            wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin((Math.PI * 2 * freq) / sampleRate * i));
+                      }
+                      break;
+
+                    case WaveForm.Square:
+                      for (int i = 0; i < sampleRate; i++)
+                      {
+                          wave[i] = Convert.ToInt16(short.MaxValue * Math.Sign(Math.Sin((Math.PI * 2 * freq) / sampleRate * i)));
+                      }
+                      break;
+                    case WaveForm.Saw:
+
+                        for (int j = 0; j < sampleRate; j++)
+                        {
+                            
+                            
+                            tempSample = -short.MaxValue;
+                            for (int i = 0; i < samplesPerWaveLength && j<sampleRate; i++)
+                            {
+                                tempSample += ampStep;
+                                wave[j++] = Convert.ToInt16(tempSample);
+                            }
+                        }
+                        break;
+                    case WaveForm.Triangle:
+
+                        tempSample = -short.MaxValue;
+                        for (int i=0;i< sampleRate;i++)
+                        {
+                            if (Math.Abs(tempSample + ampStep) > short.MaxValue)
+                            {
+                                ampStep = (short)-ampStep;
+                            }
+                            tempSample += ampStep;
+                            wave[i] = Convert.ToInt16(tempSample);
+
+                        }
+                        break;
+                    case WaveForm.Noise:
+                        for (int i = 0; i < sampleRate; i++)
+                        {
+                            wave[i] = (short)rnd.Next(-short.MaxValue, short.MaxValue);
+                        }
+                        break;
+
+                }
+
+                Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
+            }
+        // wave format standard
+            short blockAlign = bitsPerSample / 8;
+            int subChunkTwoSize = sampleRate * blockAlign;
+            MemoryStream f = new MemoryStream();
+            BinaryWriter wr = new BinaryWriter(f);
+            wr.Write(new[] {'R','I','F','F'});
+            wr.Write(36 + subChunkTwoSize);
+            wr.Write(new[] {'W','A','V','E','f','m','t',' ' });
+            wr.Write(16);
+            wr.Write((ushort)1);
+            wr.Write((ushort)1);
+            wr.Write(sampleRate);
+            wr.Write(sampleRate * blockAlign);
+            wr.Write(blockAlign);
+            wr.Write(bitsPerSample);
+            wr.Write(new[] { 'd', 'a', 't', 'a' });
+            wr.Write(subChunkTwoSize);
+            wr.Write(binaryWave);
+            f.Position = 0;
+
+        // wave player
+            new SoundPlayer(f).Play();
+
+
+        }
+
+        private void MainInterface_Load(object sender, EventArgs e)
+        {
+
+        }
+ 
+    }
+    public enum WaveForm
+    {
+        Sine, Square, Saw, Triangle, Noise
+    }
+}
